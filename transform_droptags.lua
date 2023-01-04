bce7a8f4ae110319400efcb66fa6cbce1f6f31bf
Andy Townsend <ajtown@ubuntuvm78.atownsend.org.uk>
2023-01-04 22:08:08 +0000

    Added licence file and reference GPLv3 licence in scripts.

--- transform_droptags.lua
+++ transform_droptags.lua
@@ -1,6 +1,21 @@
 --
--- If an object has name:cy, use that as name
+-- Remove tags from boundary ways that we are not interested in.
 --
+-- Copyright (C) 2022-2023  Andy Townsend
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-- -----------------------------------------------------------------------------
 
 function process(object)
     object.tags.barrier = nil
